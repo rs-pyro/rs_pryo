e6a0d9d6e355acbe4beed7d223818931a73a87a2
Jonathan Paul Wolfe <rspyrocrate@gmail.com>
2023-07-22 22:06:21 -0600

    1

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -7,11 +7,18 @@ use plotters::series::{LineSeries, PointSeries};
 
 
 // Define a simple DataPoint struct
+#[derive(Clone)]
 pub(crate) struct DataPoint {
-    pub x: f64,
-    pub y: f64,
+     x: f64,
+     y: f64,
 }
-struct LinearRegression{
+impl DataPoint{
+    pub(crate) fn new(x:f64, y:f64) ->Self{
+        DataPoint{x,y}
+    }
+}
+pub(crate) struct LinearRegression{
+    data: Box<Vec<DataPoint>>,
     coefficient: f64,
     intercept: f64,
     goodness_of_fit:f64,
@@ -23,62 +30,80 @@ struct LinearRegression{
     outliers:f64,
     homoscedasticity:f64,
 }
-// Function to calculate the linear regression coefficients
-pub(crate) fn get_slope_intercept(data: &[DataPoint]) -> (f64, f64) {
-    let n = data.len() as f64;
-    let x_sum: f64 = data.iter().map(|p| p.x).sum();
-    let y_sum: f64 = data.iter().map(|p| p.y).sum();
-    let xy_sum: f64 = data.iter().map(|p| p.x * p.y).sum();
-    let x_squared_sum: f64 = data.iter().map(|p| p.x * p.x).sum();
+impl LinearRegression {
+    pub(crate) fn new(data : &[DataPoint]) ->Self{
+        LinearRegression{
+            data: Box::new(data.to_vec()),
+            coefficient: 0.0,
+            intercept: 0.0,
+            goodness_of_fit: 0.0,
+            residuals: 0.0,
+            p_values_and_significance: 0.0,
+            confidence_intervals: 0.0,
+            predictions: 0.0,
+            multicollinearity: 0.0,
+            outliers: 0.0,
+            homoscedasticity: 0.0,
+        }
+    }
+    // Function to calculate the linear regression coefficients
+    pub(crate) fn get_slope_intercept(&mut self) -> (f64, f64) {
+        let n = self.data.len() as f64;
+        let x_sum: f64 = self.data.iter().map(|p| p.x).sum();
+        let y_sum: f64 = self.data.iter().map(|p| p.y).sum();
+        let xy_sum: f64 = self.data.iter().map(|p| p.x * p.y).sum();
+        let x_squared_sum: f64 =self.data.iter().map(|p| p.x * p.x).sum();
 
-    let slope = (n * xy_sum - x_sum * y_sum) / (n * x_squared_sum - x_sum * x_sum);
-    let intercept = (y_sum - slope * x_sum) / n;
+        self.coefficient = (n * xy_sum - x_sum * y_sum) / (n * x_squared_sum - x_sum * x_sum);
+        self.intercept = (y_sum - self.coefficient * x_sum) / n;
 
-    (slope, intercept)
-}
+        (self.coefficient, self.intercept)
+    }
 
 
 // Function to plot the data and the linear regression line
 
-pub(crate) fn plot_data_with_regression(
-    data: &[DataPoint],
-    slope: f64,
-    intercept: f64,
-    plot_title: &str,
-    x_label: &str,
-    y_label: &str,
-    output_file: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // Create a plotter area
-    let output_file = &format!("./images/plots/{}",output_file);
-    let root = BitMapBackend::new(output_file, (1600, 1200)).into_drawing_area();
-    root.fill(&WHITE)?;
+    pub(crate) fn plot_data_with_regression(
+        self: &Self,
+        data: &[DataPoint],
+        slope: f64,
+        intercept: f64,
+        plot_title: &str,
+        x_label: &str,
+        y_label: &str,
+        output_file: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        // Create a plotter area
+        let output_file = &format!("./images/plots/{}", output_file);
+        let root = BitMapBackend::new(output_file, (1600, 1200)).into_drawing_area();
+        root.fill(&WHITE)?;
 
-    // Define the plot area
-    let mut chart = ChartBuilder::on(&root)
-        .caption(plot_title, ("sans-serif", 20))
-        .margin(5)
-        .set_all_label_area_size(40)
-        .build_cartesian_2d(0.0..6.0, 0.0..6.0)?;
+        // Define the plot area
+        let mut chart = ChartBuilder::on(&root)
+            .caption(plot_title, ("sans-serif", 20))
+            .margin(5)
+            .set_all_label_area_size(40)
+            .build_cartesian_2d(0.0..6.0, 0.0..6.0)?;
 
-    // Draw the scatter plot
-    chart.configure_mesh().draw()?;
-    chart.draw_series(PointSeries::of_element(
-        data.iter().map(|p| (p.x, p.y)),
-        5,
-        &BLUE,
-        &|c, s, st| {
-            return EmptyElement::at(c) + Circle::new((0, 0), s, st.filled());
-        },
-    ))?;
+        // Draw the scatter plot
+        chart.configure_mesh().draw()?;
+        chart.draw_series(PointSeries::of_element(
+            data.iter().map(|p| (p.x, p.y)),
+            5,
+            &BLUE,
+            &|c, s, st| {
+                return EmptyElement::at(c) + Circle::new((0, 0), s, st.filled());
+            },
+        ))?;
 
-    // Draw the linear regression line
-    chart.draw_series(LineSeries::new(
-        vec![(0.0, intercept), (5.0, slope * 5.0 + intercept)],
-        &RED,
-    ))?;
+        // Draw the linear regression line
+        chart.draw_series(LineSeries::new(
+            vec![(0.0, intercept), (5.0, slope * 5.0 + intercept)],
+            &RED,
+        ))?;
 
-    Ok(())
+        Ok(())
+    }
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
-mod statistics;
 mod linear_regression;
 
-use linear_regression::get_slope_intercept;
-use linear_regression::plot_data_with_regression;
+use linear_regression::LinearRegression;
 use linear_regression::DataPoint;
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Example dataset: (x, y) pairs for the scatter plot
     let data = vec![
-        DataPoint { x: 1.0, y: 2.0 },
-        DataPoint { x: 2.0, y: 4.0 },
-        DataPoint { x: 3.0, y: 5.0 },
-        DataPoint { x: 4.0, y: 4.0 },
-        DataPoint { x: 5.0, y: 5.0 },
-        DataPoint { x: 5.0, y: 4.0 },
-        DataPoint { x: 1.5, y: 2.5 },
-        DataPoint { x: 3.2, y: 4.8 },
-        DataPoint { x: 4.8, y: 5.3 },
+        DataPoint::new( 1.0, 2.0 ),
+        DataPoint::new( 2.0,4.0 ),
+        DataPoint::new(  3.0,5.0 ),
+        DataPoint::new(4.0, 4.0 ),
+        DataPoint::new( 5.0, 5.0 ),
+        DataPoint::new( 5.0, 4.0 ),
+        DataPoint::new( 1.5, 2.5 ),
+        DataPoint::new(3.2, 4.8 ),
+        DataPoint::new(4.8, 5.3 ),
     ];
-
+    let mut lr = LinearRegression::new(&data);
     // Calculate the linear regression coefficients
-    let (slope, intercept) = get_slope_intercept(&data);
+    let (slope, intercept) = lr.get_slope_intercept();
 
     // Print the results
     println!("Slope (coefficient): {}", slope);
     println!("intercept: {}", intercept);
 
     // Plot the data with the linear regression line
-    plot_data_with_regression(
+    lr.plot_data_with_regression(
         &data,
         slope,
         intercept,
